05e5ac0e5d0befe41ec3b826e0dfbb6b96f360d1
Toru Choi <blueocean239@gmail.com>
2021-07-18 21:31:43 +0800

    feat: add kingfisher

--- Sources/FirstSPMLibrart/FirstSPMLibrart.swift
+++ Sources/FirstSPMLibrart/FirstSPMLibrart.swift
@@ -1,3 +1,20 @@
 struct FirstSPMLibrart {
     var text = "Hello, World!"
 }
+
+class AlamofireBundle {
+    let URL = "https://jsonplaceholder.typicode.com/todos/1"
+    
+    init() {
+        // Initialize Alamofire
+    }
+}
+
+
+
+struct FakeJsonResponse: Codable {
+    let userId: Int
+    let id: Int
+    let title: String
+    let completed: Bool
+}
